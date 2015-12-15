6fa7b202a2f3fada36af9c85b1bc3aa52069a0cc
joao-pedro-alves <joaopedroalv9@gmail.com>
2015-12-15 14:52:15 -0200

    correcao do nome do autor

--- wp-seo-imgly.php
+++ wp-seo-imgly.php
@@ -4,7 +4,7 @@ Plugin Name: DOM SEO Image
 Plugin URI: https://github.com/joao-pedro-alves/dom-seo-image
 Description: Melhore o tráfego dos buscadores dando atenção as imagens do seu Wordpress. DOM SEO Image gera automaticamente as tags TITLE e ALT de suas imagens de um maneira totalmente configurável e simples
 Version: 1.0.1
-Author: João Pedro Alves & Domenico Alario
+Author: João Pedro Alves & Doménico Alario
 */	
 
 require_once( dirname( __FILE__ ) . '/wp-seo-imgly-main.php' );
