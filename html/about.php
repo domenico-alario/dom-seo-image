bd1b58df03a1bc3396c7e08c8daf578716d0a0a8
joao-pedro-alves <joaopedroalv9@gmail.com>
2015-12-15 14:45:30 -0200

    correcao na página de sobre

--- html/about.php
+++ html/about.php
@@ -1,8 +1,7 @@
 <h2><?= $this->name ?> - <b>Sobre</b></h2>
 <div class="postbox">
 	<div class="inside">
-	<p><b>Muito obrigado por nos confiar suas imagens! Nós do  aceditamos que imagens contam histórias e nosso trabalho ajuda que mais pessoas possam saber sobre suas histórias.</b></p>
-	<p>Muito obrigado por nos confiar suas imagens! Nós do <?= $this->name ?> acreditamos que imagens contam histórias e nosso trabalho ajuda que mais pessoas possam saber sobre suas histórias.</p>
+	<p><b>Muito obrigado por nos confiar suas imagens! Nós do <?= $this->name ?> acreditamos que imagens contam histórias e nosso trabalho ajuda que mais pessoas possam saber sobre suas histórias.</b></p>
 	<p>Este plugin foi desenvolvido com os ultimos recursos de atualização e as melhores técnicas em SEO para que o Google indexe suas imagens nos buscadores e aumente o ranking de suas páginas e posts.</p>
 	<hr />
 	<p>Desenvolvido por: <i>João Pedro Alves</i> & <i>Doménico Alario</i></p>
