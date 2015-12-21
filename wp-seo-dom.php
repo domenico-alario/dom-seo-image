53d759707170e3244b5283a9ea2b63f5be519c6b
joao-pedro-alves <joaopedroalv9@gmail.com>
2015-12-21 14:02:20 -0200

    alterando versao

--- wp-seo-dom.php
+++ wp-seo-dom.php
@@ -3,7 +3,7 @@
 Plugin Name: DOM SEO Image
 Plugin URI: https://github.com/joao-pedro-alves/dom-seo-image
 Description: Melhore o tráfego dos buscadores dando atenção as imagens do seu Wordpress. DOM SEO Image gera automaticamente as tags TITLE e ALT de suas imagens de um maneira totalmente configurável e simples
-Version: 1.0.1
+Version: 1.0.2
 Author: João Pedro Alves & Doménico Alario
 */	
 
